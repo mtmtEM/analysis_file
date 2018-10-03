ad3cd8ff14746cc29cf86bee67b7adab1324e204
mtmt <>
2018-10-03 18:16:31 +0900

    update: Readme, program exec

--- Readme.md
+++ Readme.md
@@ -1,3 +1,17 @@
 #######################
 # ファイル解析用プログラム#
 #######################
+
+# install modules
+
+## python version
+```
+$ python -V
+Python 3.6.4
+```
+
+## pip modules
+```
+$ pip install memory_profiler
+$ pip install mysqlclient
+```
--- py/main.py
+++ py/main.py
@@ -1,19 +1,30 @@
 import json
+import sys, os
 from memory_profiler import profile
 
 class ReadFiles:
     def __init__(self,path):
         self.path = path
 
-    # @profile
+    @profile
     def read_data(self):
         with open(self.path) as f:
             data = json.load(f)
             print(data)
 
 if __name__ == '__main__':
-    print("<---start--->")
-    path = '../files/sample.json'
+    # path = '../files/sample.json'
+    args = sys.argv
+
+    if len(args) != 2:
+        print("Incorrect args. ($ python3 main.py <File_path.json>)")
+        sys.exit()
+    elif os.path.exists(args[1]) == False:
+        print("No such file or directory.")
+        sys.exit()
+    else:
+        path = args[1]
+
     target = ReadFiles(path)
     target.read_data()
-    print("<---fin.--->")
+    print("fin.")
