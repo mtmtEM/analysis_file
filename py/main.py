8e86f6b5095b1e8b5d2d3c6272d268e039e28477
mtmt <>
2018-10-03 17:51:47 +0900

    update: json style

--- py/main.py
+++ py/main.py
@@ -5,15 +5,15 @@ class ReadFiles:
     def __init__(self,path):
         self.path = path
 
-    @profile
+    # @profile
     def read_data(self):
         with open(self.path) as f:
             data = json.load(f)
             print(data)
 
 if __name__ == '__main__':
-    print("<start>")
+    print("<---start--->")
     path = '../files/sample.json'
     target = ReadFiles(path)
     target.read_data()
-    print("<fin.>")
+    print("<---fin.--->")
